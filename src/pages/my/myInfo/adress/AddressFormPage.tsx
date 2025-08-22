8da2f39b63e20bcab5336692aced6a951bea061e
hyahn0 <gmldbs0725@gmail.com>
2025-08-22 14:08:28 +0900

    MNMS-405 feat: 배송지 추가

--- src/pages/my/myInfo/adress/AddressFormPage.tsx
+++ src/pages/my/myInfo/adress/AddressFormPage.tsx
@@ -1,12 +1,19 @@
+// src/pages/my/myinfo/AddressFormPage.tsx
 import React from 'react';
 import AddressForm from '@/components/my/myinfo/AddressForm';
 import styles from './AddressFormPage.module.css';
+import MyHeader from '@/components/my/hedaer/MyHeader';
 
 const AddressFormPage: React.FC = () => {
   return (
-    <section className={styles.container}>
-      <h2 className={styles.title}>배송지 추가</h2>
-      <AddressForm />
+    <section className={styles.page}>
+      <div className={styles.top}>
+        <MyHeader title="배송지 추가" />
+      </div>
+
+      <div className={styles.body}>
+        <AddressForm />
+      </div>
     </section>
   );
 };
