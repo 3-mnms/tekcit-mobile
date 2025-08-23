4d1a5b60b9664415ebf47ec6db1621236c88e548
hyahn0 <gmldbs0725@gmail.com>
2025-08-23 14:47:17 +0900

    Run npm run build 에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import path from 'path';
-
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
